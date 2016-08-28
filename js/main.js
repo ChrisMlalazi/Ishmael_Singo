37391df7d976d8a730837665bdcfc8dda3b6ca53
Christopher Mlalazi <chrismlalazi@gmail.com>
2016-08-27 19:02:12 -0500

    CSS

--- js/main.js
+++ js/main.js
@@ -8,16 +8,5 @@ $('.navicon').on('click', function (e) {
 
 
 
-$(document).ready(function() {
 
-  var weatherAPI = 'http://api.openweathermap.org/data/2.5/weather';
-  var data = {
-    q : "Portland,OR",
-    units : "metric"
-  };
-  function showWeather(weatherReport) {
-    $('#temperature').text(weatherReport.main.temp);
-  }
-  $.getJSON(weatherAPI, data, showWeather);
-});
 
